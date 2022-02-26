54f1d964ca818cd71a18f2f79f0a04ed338a5b4b
joelvitorniino <joelprofessor13@gmail.com>
2022-02-26 01:35:32 -0300

    rollback of fixing error

--- src/controllers/GetInfoVideoController.ts
+++ src/controllers/GetInfoVideoController.ts
@@ -1,18 +1,11 @@
 import ytdl from "ytdl-core";
 import { Request, Response } from 'express';
-import axios from 'axios';
-
-interface IVideo {
-    links: string[];
-};
 
 class GetInfoVideoController {
     async getInfoVideo(request: Request, response: Response) {    
         const { link } = request.query;
 
-        const responseLink = await axios.get<IVideo>(`https://gcv-api-bot.herokuapp.com/search_video?q=${link}`);
-
-        ytdl.getInfo(responseLink.data.links[0])
+        ytdl.getInfo(link as string)
             .then(({ formats }) => {
                 const { url } = formats[0];
 
