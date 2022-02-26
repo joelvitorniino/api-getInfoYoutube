aaac2446cbcc805874fa72eb3822974b940f4b16
joelvitorniino <joelprofessor13@gmail.com>
2022-02-26 02:18:27 -0300

    fix error of url youtube mobile

--- src/controllers/GetInfoMusicController.ts
+++ src/controllers/GetInfoMusicController.ts
@@ -1,10 +1,13 @@
 import { Request, Response } from 'express';
 import ytdl from 'ytdl-core';
+import { youtube_parser } from '../utils/youtube_parser';
 class GetInfoMusicController {
     async getInfoMusic(request: Request, response: Response) {
         const { link } = request.query; 
+        const getIDMusic = youtube_parser(link as string);
+        const link_parsed_url = `https://youtube.com/watch?v=${getIDMusic}`;
 
-        const info = await ytdl.getInfo(link as string);
+        const info = await ytdl.getInfo(link_parsed_url);
         const audioFormats = ytdl.filterFormats(info.formats, 'audioonly');
         const urlMusic = audioFormats[0].url;
 
--- src/controllers/GetInfoVideoController.ts
+++ src/controllers/GetInfoVideoController.ts
@@ -1,11 +1,14 @@
 import ytdl from "ytdl-core";
 import { Request, Response } from 'express';
+import { youtube_parser } from "../utils/youtube_parser";
 
 class GetInfoVideoController {
     async getInfoVideo(request: Request, response: Response) {    
         const { link } = request.query;
+        const getIDVideo = youtube_parser(link as string);
+        const link_parsed_url = `https://youtube.com/watch?v=${getIDVideo}`;
 
-        ytdl.getInfo(link as string)
+        ytdl.getInfo(link_parsed_url)
             .then(({ formats }) => {
                 const { url } = formats[0];
 
